f97f1cc99ce913522539484bad506a85a5d74a3c
Rogerio Prado de Jesus <rogerio.p.jesus@caixa.gov.br>
2021-10-06 02:41:19 -0300

    Renomeia binario para tratar cloud policy
    Não é permitido armazenar arquivos .exe na plataforma de nuvem utilizada na
    empresa.
    
    Com este ajuste, as sincronizações voltam a funcionar.

--- protecao-tela-aumentar-tempo.bat
+++ protecao-tela-aumentar-tempo.bat
@@ -5,4 +5,4 @@ set TEMPO_EM_SEGUNDOS=599940
 
 Reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveTimeOut /t REG_SZ /d %TEMPO_EM_SEGUNDOS% /f
 
-SetScrTimeout.exe %TEMPO_EM_SEGUNDOS%
+vendor\SetScrTimeout.exec %TEMPO_EM_SEGUNDOS%
